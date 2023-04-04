4d987b76360057de0e1035f270be61eb74d48bcb
RCGEOS <127776140+rcgeos@users.noreply.github.com>
2023-04-04 09:25:52 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pypackage',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for a Python package',
+    author='RCGeos',
+    license='BSD',
+    author_email='renato.cumani4@gmail.com',
+    url='https://github.com/rcgeos/pypackage',
+    keywords=['cookiecutter', 'template', 'package', 'mkdocs'],
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
